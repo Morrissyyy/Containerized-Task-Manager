bd7e68a857c7f8f1343f857cb45c39fb777f06c3
Michielvwijk <michiel.vanwijk@student.vonknh.nl>
2025-09-09 09:55:01 +0200

    edit task working

--- backend/app.py
+++ backend/app.py
@@ -44,6 +44,14 @@ def delete_task(task_id):
     db.session.delete(task)
     db.session.commit()
     return jsonify({'message': f'Task {task_id} deleted'})
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    data = request.get_json()
+    task.title = data.get('title', task.title)
+    task.description = data.get('description', task.description)
+    db.session.commit()
+    return jsonify({'id': task.id, 'title': task.title, 'description': task.description})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
